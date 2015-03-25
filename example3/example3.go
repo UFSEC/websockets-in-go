dda707ef9bc7a95e9a28497bd30dc3a72a483c3f
Baker Bokorney <bbokorney@gmail.com>
2015-03-25 15:33:05 -0400

    Removed some debug printing. Spectators are allowed. And, after one game ends the next one starts.

--- example3/example3.go
+++ example3/example3.go
@@ -57,31 +57,41 @@ var updatesInc chan Update = make(chan Update, 100)
 var newClient chan bool = make(chan bool, 10)
 var newUpdateChan chan chan Message = make(chan chan Message, 10)
 
+func updateAll(msg Message, cop chan Message, rob chan Message, spectators []chan Message) {
+    cop <- msg
+    rob <- msg
+    for _, specChan := range spectators {
+        specChan <- msg
+    }
+}
+
 func stateManager() {
     var cop *Player = nil
     var rob *Player = nil
     var moveCount int = 100
+    var spectators []chan Message
 
     for {
         select {
             case <-newClient:
-                log.Println("New client")
                 if rob == nil {
-                    log.Println("Creating the robber")
                     rob = &Player{Position{0,0}, make(chan Message, 100)}
                     newUpdateChan <- rob.update
                     rob.update <- Message{"init", Init{"rob", rob.pos}}
                 } else if cop == nil {
-                    log.Println("Creating the cop")
                     cop = &Player{Position{9,9}, make(chan Message, 100)}
                     newUpdateChan <- cop.update
                     cop.update <- Message{"init", Init{"cop", cop.pos}}
                     // both players are here, let's start the game!
                     msg := Message{"start", GameStart{cop.pos, rob.pos}}
-                    rob.update <- msg
-                    cop.update <- msg
+                    updateAll(msg, cop.update, rob.update, spectators)
+                    // rob.update <- msg
+                    // cop.update <- msg
                 } else { // send a nil to indicate we've already got our 2 players
-                    newUpdateChan <- nil
+                    newChan := make(chan Message, 100)
+                    newUpdateChan <- newChan
+                    newChan <- Message{"init", Init{"spectator", Position{0,0}}}
+                    spectators = append(spectators, newChan)
                 }
             case u := <-updatesInc:
                 if u.Who == "rob"{
@@ -93,15 +103,19 @@ func stateManager() {
                 if rob.pos.Row == cop.pos.Row && rob.pos.Col == cop.pos.Col {
                     // the game is over, cop wins!
                     msg := Message{"winner", GameOver{"cop", cop.pos}}
-                    cop.update <- msg
-                    rob.update <- msg
+                    // cop.update <- msg
+                    // rob.update <- msg
+                    updateAll(msg, cop.update, rob.update, spectators)
+                    log.Println("Game over!")
                     return
                 }
                 if moveCount == 0 {
                     // the game is over, robber wins!
                     msg := Message{"winner", GameOver{"rob", rob.pos}}
-                    cop.update <- msg
-                    rob.update <- msg
+                    // cop.update <- msg
+                    // rob.update <- msg
+                    updateAll(msg, cop.update, rob.update, spectators)
+                    log.Println("Game over!")
                     return
                 }
 
@@ -110,15 +124,18 @@ func stateManager() {
                     // if it is a cop who moved, we send
                     // a modified move cout
                     msg := Message{"update", Update{"cop", cop.pos}}
-                    cop.update <- msg
-                    rob.update <- msg
+                    // cop.update <- msg
+                    // rob.update <- msg
+                    updateAll(msg, cop.update, rob.update, spectators)
                     msg = Message{"count", MoveCountUpdate{moveCount}}
-                    cop.update <- msg
-                    rob.update <- msg
+                    // cop.update <- msg
+                    // rob.update <- msg
+                    updateAll(msg, cop.update, rob.update, spectators)
                 } else if u.Who == "rob" {
                     msg := Message{"update", Update{"rob", rob.pos}}
-                    cop.update <- msg
-                    rob.update <- msg
+                    // cop.update <- msg
+                    // rob.update <- msg
+                    updateAll(msg, cop.update, rob.update, spectators)
                 }
             } 
     }
@@ -135,11 +152,8 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
     defer func(){ ws.Close() }()
 
-    log.Println("Asking for data chan")
     newClient <- true
-    log.Println("Waiting for data chan")
     updateOut := <-newUpdateChan
-    log.Println("Got my data chan!")
     if updateOut == nil { // we've already got 2 players
         fmt.Sprint(w, "Sorry, this game is full!")
         return
@@ -179,7 +193,11 @@ func homeHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-    go stateManager()
+    go func(){
+        for {
+            stateManager()
+        }
+    }() 
 
     http.HandleFunc("/", homeHandler)
     http.HandleFunc("/ws", wsHandler)
